3fb8973580cf849802e32732a2e5319f51a901c2
Milan Radojevic <mikister2012@gmail.com>
2019-10-13 18:26:15 +0200

    Add build

---
+++ build/precache-manifest.008ef95c0676d86333692cfa33b13861.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd804d9dadd22614bc3d2212658a5fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af0cf92507631f8067d",
+    "url": "/static/css/main.93060c0b.chunk.css"
+  },
+  {
+    "revision": "15da8d23cbf6c058b725",
+    "url": "/static/js/2.dfd8a019.chunk.js"
+  },
+  {
+    "revision": "7af0cf92507631f8067d",
+    "url": "/static/js/main.bf02503e.chunk.js"
+  },
+  {
+    "revision": "8f273ebe1f1db95b1013",
+    "url": "/static/js/runtime-main.9704bcf0.js"
+  }
+]);
